bfc6790424ecb80f4f8942e878a70c75ef68d7bb
NicEastvillage <nicoesterby@gmail.com>
2019-10-03 13:50:42 +0200

    Refactored to use abstraction instead of blob for RabbitMQ interaction

--- src/frontier.rs
+++ src/frontier.rs
@@ -1,10 +1,11 @@
 use crate::task::Task;
-use lapin_futures::{Channel, Client, ConnectionProperties, ExchangeKind};
-use lapin_futures::options::{BasicPublishOptions, QueueDeclareOptions, ExchangeDeclareOptions, QueueBindOptions, BasicConsumeOptions, QueuePurgeOptions};
+use lapin_futures::{Channel, Client, ConnectionProperties, ExchangeKind, Queue, BasicProperties};
+use lapin_futures::options::{BasicPublishOptions, QueueDeclareOptions, ExchangeDeclareOptions, QueueBindOptions, BasicConsumeOptions};
 use lapin_futures::types::FieldTable;
 use futures::Future;
+use tokio::prelude::Stream;
 
-pub trait Queue {
+pub trait Frontier {
     fn submit_task(&self, task: Task) -> Result<(), ()>;
 
     fn subscribe(&self, f: Box<dyn Fn(Task) -> TaskSubmitResult>);
@@ -18,12 +19,13 @@ pub enum TaskSubmitResult {
     Reject,
 }
 
-pub struct RabbitmqQueue {
+pub struct RabbitmqFrontier {
     channel: Channel,
+    queue: Queue,
 }
 
-impl RabbitmqQueue {
-    fn new(addr: String) -> Result<Self, ()> {
+impl RabbitmqFrontier {
+    pub(crate) fn new(addr: String) -> Result<Self, ()> {
         match Client::connect(&addr, ConnectionProperties::default()).and_then(|client| {
             // create_channel returns a future that is resolved
             // once the channel is successfully created
@@ -37,7 +39,7 @@ impl RabbitmqQueue {
             // the channel
             channel.queue_declare("frontier",
                                   QueueDeclareOptions::default(),
-                                  FieldTable::default()).and_then( move |queue| {
+                                  FieldTable::default()).and_then(move |queue| {
                 println!("channel {} declared queue 'frontier'", id);
 
                 channel.exchange_declare("work",
@@ -53,8 +55,9 @@ impl RabbitmqQueue {
                                        FieldTable::default()).and_then(move |_| {
                         println!("channel {} bound 'work' to 'frontier'", id);
 
-                        Ok(RabbitmqQueue {
-                            channel
+                        Ok(RabbitmqFrontier {
+                            channel,
+                            queue,
                         })
                     })
                 })
@@ -66,16 +69,41 @@ impl RabbitmqQueue {
     }
 }
 
-impl Queue for RabbitmqQueue {
+impl Frontier for RabbitmqFrontier {
     fn submit_task(&self, task: Task) -> Result<(), ()> {
-        unimplemented!()
+        match self.channel.basic_publish("work", "", task.serialise(), BasicPublishOptions::default(), BasicProperties::default()).wait() {
+            Ok(_) => Ok(()),
+            Err(_) => Err(()),
+        }
     }
 
     fn subscribe(&self, f: Box<dyn Fn(Task) -> TaskSubmitResult>) {
-        unimplemented!()
+        self.channel.basic_consume(&self.queue, "my_consumer", BasicConsumeOptions::default(), FieldTable::default()).and_then(
+            move |consumer| {
+                consumer.for_each(move |dev| {
+                    let task = Task::deserialise(dev.data);
+                    let result = f(task);
+                    match result {
+                        TaskSubmitResult::Ok => {
+                            self.channel.basic_ack(dev.delivery_tag, false)
+                        }
+                        TaskSubmitResult::Reject => {
+                            // No op, so the task is rescheduled
+                            unimplemented!("Ended in Reject-state")
+                        }
+                        TaskSubmitResult::Err => {
+                            panic!("Processing task failed")
+                        }
+                    }
+                })
+            }
+        ).wait().expect("Subscription failed");
     }
 
     fn close(self) -> Result<(), ()> {
-        unimplemented!()
+        match self.channel.close(0u16, "Client closed connection").wait() {
+            Ok(_) => Ok(()),
+            Err(_) => Err(())
+        }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -5,80 +5,26 @@ extern crate lapin_futures;
 mod frontier;
 mod task;
 
-use futures::future::Future;
-use lapin_futures as lapin;
-use crate::lapin::ExchangeKind;
-use crate::lapin::{BasicProperties, Client, ConnectionProperties};
-use crate::lapin::options::{BasicPublishOptions, QueueDeclareOptions, ExchangeDeclareOptions, QueueBindOptions, BasicConsumeOptions, QueuePurgeOptions};
-use crate::lapin::types::FieldTable;
-use tokio::prelude::Stream;
-use lapin_futures::Channel;
 use crate::task::Task;
+use crate::frontier::{RabbitmqFrontier, Frontier, TaskSubmitResult};
+use std::error::Error;
 
-fn main() {
+fn main() -> Result<(), Box<dyn Error>> {
     let addr = "amqp://192.168.99.100:5672/%2f";
 
     let msg = Task { url: String::from("https://aau.dk") };
 
-    futures::executor::spawn(
-        Client::connect(&addr, ConnectionProperties::default()).and_then(|client| {
-            // create_channel returns a future that is resolved
-            // once the channel is successfully created
-            client.create_channel()
-        }).and_then(|channel| {
-            let id = channel.id();
-            println!("created channel with id: {}", id);
+    let frontier = RabbitmqFrontier::new(addr.to_string()).unwrap();
+    frontier.submit_task(msg).unwrap();
 
-            // we using a "move" closure to reuse the channel
-            // once the queue is declared. We could also clone
-            // the channel
-            channel.queue_declare("frontier",
-                                  QueueDeclareOptions::default(),
-                                  FieldTable::default()).and_then(move |queue| {
-                println!("channel {} declared queue 'frontier'", id);
-                channel.queue_purge("frontier", QueuePurgeOptions::default());
+    let f = |msg: Task| {
+        println!("Task has url: {}", msg.url);
+        TaskSubmitResult::Ok
+    };
 
-                channel.exchange_declare("work",
-                                         ExchangeKind::Fanout,
-                                         ExchangeDeclareOptions::default(),
-                                         FieldTable::default()).and_then(move |_| {
-                    println!("channel {} declared exchange 'work'", id);
+    frontier.subscribe(Box::from(f));
 
-                    channel.queue_bind("frontier",
-                                       "work",
-                                       "",
-                                       QueueBindOptions::default(),
-                                       FieldTable::default()).and_then(move |_| {
-                        println!("channel {} bound 'work' to 'frontier'", id);
+    frontier.close().expect("Could not close subscription");
 
-                        channel.basic_publish("work", "", msg.serialise(),
-                                              BasicPublishOptions::default(), BasicProperties::default()).wait().expect("publish");
-
-                        channel.basic_consume(&queue,
-                                              "my_consumer",
-                                              BasicConsumeOptions::default(),
-                                              FieldTable::default()).and_then(move |consumer| {
-                            println!("channel {} created consumer", id);
-
-                            // Runs until error occurs
-                            consumer.for_each(move |dev| {
-                                let task = Task::deserialise(dev.data);
-                                println!("msg: {}", task.url);
-
-                                let first_new_task = Task { url: format!("{}/0", task.url) };
-                                let second_new_task = Task { url: format!("{}/1", task.url) };
-
-                                channel.basic_publish("work", "", first_new_task.serialise(),
-                                                      BasicPublishOptions::default(), BasicProperties::default()).wait().expect("publish");
-                                channel.basic_publish("work", "", second_new_task.serialise(),
-                                                      BasicPublishOptions::default(), BasicProperties::default()).wait().expect("publish");
-
-                                channel.basic_ack(dev.delivery_tag, false)
-                            })
-                        })
-                    })
-                })
-            })
-        })
-    ).wait_future().expect("runtime failure");
+    Ok(())
 }
