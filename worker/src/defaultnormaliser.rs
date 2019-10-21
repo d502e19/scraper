cbbbc7d33baba76c3e199e9dda470acfc8740b51
larsb <lfryd17@student.aau.dk>
2019-10-21 13:26:33 +0200

    Fixed issues with last push.

--- worker/src/defaultnormaliser.rs
+++ worker/src/defaultnormaliser.rs
@@ -1,49 +1,66 @@
+use crate::traits::Normaliser;
+use crate::task::Task;
 
 use url::{Url, ParseError};
 use url_normalizer::normalize;
 use std::error::Error;
 use tokio::io::ErrorKind;
 use std::fmt;
-use futures::future::ok;
 
-pub struct DefaultNormaliser {}
+pub struct DefaultNormaliser;
 
 impl DefaultNormaliser {
     fn full_normalisation(&self, url: Url) -> Result<Url, ()> {
-        let new_url = url;
+        let mut new_url = url;
 
         //Normalising by ordering the query in alphabetic order,
         //removes hash from url and changes encrypted to unencrypted.
         url_normalizer::normalize(new_url)
     }
 
-    fn scheme_and_host_to_lowercase(&self, url:Url) -> Result<Url, ()>{
+    fn scheme_and_host_to_lowercase(&self, url: Url) -> Result<Url, ()> {
         let mut new_url = url;
 
-        let host = new_url.host_str().unwrap().to_lowercase();
-        let scheme = new_url.scheme().to_lowercase();
+        let host = url.host_str().unwrap().to_lowercase();
+        let scheme = url.scheme().to_lowercase();
 
         new_url.set_scheme(&scheme);
         new_url.set_host(Option::Some(&host));
 
         Ok(new_url)
-
     }
 
     fn removing_dots_in_path(&self, url: Url) -> Result<Url, ()> {
         let mut new_url = url;
 
-        let mut path = new_url.path().to_string();
+        let mut path = url.path().to_string();
         path = path
             .replace("../", "")
             .replace("./", "");
 
-        new_url.set_path(&path);
+        new_url.set_path(path.as_str());
 
         Ok(new_url)
     }
 }
 
+impl Normaliser for DefaultNormaliser {
+    fn normalise(&self, task: Task) -> Result<Task, Box<dyn Error>> {
+
+        match self.full_normalisation(task.url) {
+            Ok(url) => {
+                Ok(
+                    Task {
+                        url
+                    }
+                )
+            }
+            Err(_) => {
+                Err(Box::new(NormaliseError("Normalisation went wrong.".into())))
+            }
+        }
+    }
+}
 
 #[derive(Debug)]
 struct NormaliseError(String);
@@ -53,4 +70,5 @@ impl fmt::Display for NormaliseError {
         write!(f, "There is an error: {}", self.0)
     }
 }
+
 
