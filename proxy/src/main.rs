b63d3fca976142b31d3bd1e1314369e8a0351f0c
ebje17 <34238291+ebje17@users.noreply.github.com>
2019-10-23 14:27:18 +0200

    Added comments and deleted basic_publish

--- proxy/src/main.rs
+++ proxy/src/main.rs
@@ -8,70 +8,58 @@ mod task;
 use std::error::Error;
 use redis::Commands;
 use redis::RedisResult;
-use std::collections::HashSet;
 use futures::future::Future;
 use futures::stream::Stream;
 use lapin_futures::options::{
-    BasicConsumeOptions, BasicPublishOptions, BasicRejectOptions, QueueDeclareOptions, ExchangeDeclareOptions, QueueBindOptions
+    BasicConsumeOptions, BasicRejectOptions, QueueDeclareOptions, ExchangeDeclareOptions, QueueBindOptions,
 };
 use lapin_futures::types::FieldTable;
-use lapin_futures::{BasicProperties, Channel, Client, ConnectionProperties, Queue, ExchangeKind};
-use futures::IntoFuture;
-use std::io::BufRead;
-use std::sync::mpsc::channel;
-use std::str::from_utf8;
-use std::str;
-use std::string::String;
+use lapin_futures::{Client, ConnectionProperties, ExchangeKind};
 use crate::task::Task;
 
 
 fn main() -> Result<(), Box<dyn Error>> {
+    // tries to get a connection to redis
+    // if a connection is established continue else there is an error
     let client = redis::Client::open("redis://192.168.99.100:6379/").unwrap();
-    let mut con = client.get_connection();
-    let exptask = Task { url :  "aau.dk".to_string()};
+    let con = client.get_connection();
     match con {
-        Ok(mut connection ) => {
+        Ok(mut connection) => {
+            // establish a connection to rabbitMQ
             let addr = std::env::var("AMQP_ADDR").unwrap_or_else(|_| "amqp://192.168.99.100:5672/%2f".into());
-
             futures::executor::spawn(
-            Client::connect(&addr, ConnectionProperties::default()).and_then(|client| {
-                println!("CONNECTED");
-              client.create_channel().and_then(|channel| {
-                  channel.exchange_declare("work", ExchangeKind::Fanout,
-                                           ExchangeDeclareOptions::default(), FieldTable::default());
-                  channel.queue_declare("collection", QueueDeclareOptions::default(),
-                                        FieldTable::default(),).and_then(move |queue |{
-                      channel.queue_bind("collection", "work", "",
-                                         QueueBindOptions::default(), FieldTable::default());
-                      channel.basic_publish("work", "", exptask.serialise(),  BasicPublishOptions::default(),
-                                            BasicProperties::default(),).wait();
-
-                      channel.basic_consume(&queue, "", BasicConsumeOptions::default(),
-                                                                 FieldTable::default(),).and_then(|consumer|{
-                              consumer.for_each(move |msg| {
-                                  let task = Task::deserialise(msg.data);
-                                  let add_res: RedisResult<u32> = connection.sadd("collection", &task);
-                                  if let Ok(s) = add_res {
-                                      channel.basic_ack(msg.delivery_tag, false)
-                                  } else {
-                                      channel.basic_reject(
-                                          msg.delivery_tag,
-                                          BasicRejectOptions::default(), //TODO
-                                      )
-                                  }
-                              })
-
-
-                          })
-                  })
-              })
-            })
-
-            ).wait_future().expect("error");
+                Client::connect(&addr, ConnectionProperties::default()).and_then(|client| {
+                    // finds collection and sees the tasks
+                    client.create_channel().and_then(|channel| {
+                        channel.exchange_declare("work", ExchangeKind::Fanout,
+                                                 ExchangeDeclareOptions::default(), FieldTable::default());
+                        channel.queue_declare("collection", QueueDeclareOptions::default(),
+                                              FieldTable::default(), ).and_then(move |queue| {
+                            channel.queue_bind("collection", "work", "",
+                                               QueueBindOptions::default(), FieldTable::default());
+                            channel.basic_consume(&queue, "", BasicConsumeOptions::default(),
+                                                  FieldTable::default(), ).and_then(|consumer| {
+                                // copies every task from collection to redis
+                                consumer.for_each(move |msg| {
+                                    let task = Task::deserialise(msg.data);
+                                    let add_res: RedisResult<u32> = connection.sadd("collection", &task);
+                                    if let Ok(s) = add_res {
+                                        channel.basic_ack(msg.delivery_tag, false)
+                                    } else {
+                                        channel.basic_reject(
+                                            msg.delivery_tag,
+                                            BasicRejectOptions::default(), //TODO
+                                        )
+                                    }
+                                })
+                            })
+                        })
+                    })
+                })
+            ).wait_future().expect("Could not connect to rabbitMQ");
         }
         Err(_) =>
-            println!("error"),
+            println!("Could not connect to redis"),
     }
-
     Ok(())
 }
