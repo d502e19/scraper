eef192a7905baf16a83e03e60194889f698b7982
cogitantium <falke.cs@gmail.com>
2019-10-24 15:16:36 +0200

    Implemented further arguments

--- worker/src/main.rs
+++ worker/src/main.rs
@@ -5,6 +5,20 @@ extern crate rand;
 extern crate redis;
 extern crate tokio;
 
+use std::collections::HashSet;
+use std::error::Error;
+
+use clap::{App, Arg};
+use redis::Commands;
+
+use crate::downloader::DefaultDownloader;
+use crate::extractor::html::{HTMLExtractorBase, HTMLLinkExtractor};
+use crate::rmqredis::RMQRedisManager;
+use crate::task::Task;
+use crate::traits::Downloader;
+use crate::void::Void;
+use crate::worker::Worker;
+
 mod downloader;
 mod extractor;
 mod rmqredis;
@@ -14,21 +28,9 @@ mod traits;
 mod void;
 mod worker;
 
-use crate::downloader::DefaultDownloader;
-use crate::extractor::html::{HTMLExtractorBase, HTMLLinkExtractor};
-use crate::rmqredis::RMQRedisManager;
-use crate::task::Task;
-use crate::traits::Downloader;
-use crate::void::Void;
-use crate::worker::Worker;
-use clap::{App, Arg};
-use redis::Commands;
-use std::collections::HashSet;
-use std::error::Error;
-
 fn main() -> Result<(), Box<dyn Error>> {
     let args = App::new("DatScraper")
-        .version("0.1")
+        .version("0.1.0")
         .author("d502e19@aau")
         .arg(
             Arg::with_name("config")
@@ -44,10 +46,34 @@ fn main() -> Result<(), Box<dyn Error>> {
             .help("Specify the redis-port to connect to")
     ).arg(
         Arg::with_name("rabbitmq-port")
-            .short("q")
+            .short("p")
             .long("rmq-port")
             .value_name("PORT")
-            .help("Specify the RabbitMQ-port to connect to")
+            .help("Specify the RabbitMQ port to connect to")
+    ).arg(
+        Arg::with_name("rabbitmq-exchange")
+            .short("e")
+            .long("rmq-exchange")
+            .value_name("EXCHANGE")
+            .help("Specify the RabbitMQ exchange to connect to")
+    ).arg(
+        Arg::with_name("rabbitmq-routing-key")
+            .short("k")
+            .long("rmq-routing-key")
+            .value_name("KEY")
+            .help("Specify the RabbitMQ routing-key to connect to")
+    ).arg(
+        Arg::with_name("rabbitmq-queue")
+            .short("q")
+            .long("rmq-queue")
+            .value_name("QUEUE")
+            .help("Specify the RabbitMQ queue to connect to")
+    ).arg(
+        Arg::with_name("redis-set")
+            .short("s")
+            .long("redis-set")
+            .value_name("SET")
+            .help("Specify the redis set to connect to")
     ).arg(
         Arg::with_name("manager-address")
             .short("a")
@@ -56,11 +82,12 @@ fn main() -> Result<(), Box<dyn Error>> {
             .help("Specify the manager's address")
     ).get_matches();
     // Construct a worker and its components
+    println!("{:?}", args.default_val)
     let manager = RMQRedisManager::new(
         args.value_of("manager-address").unwrap_or("localhost").to_string(),
-        5672,
-        6379,
-        "work".to_string(),
+        args.value_of("rabbitmq-port").unwrap_or("5672").parse().unwrap(),
+        args.value_of("redis-port").unwrap_or("6379").parse().unwrap(),
+        args.value_of("rabbitmq-exchange").unwrap_or("work").to_string(),
         "".to_string(),
         "frontier".to_string(),
         "collection".to_string(),
