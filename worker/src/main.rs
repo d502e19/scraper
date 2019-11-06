5f46fb3f0369e9961087ba5b47921605264ffabe
cogitantium <falke.cs@gmail.com>
2019-11-06 15:02:19 +0100

    [proxy] Rework proxy, improve parser, correct RMQ-address, etc.

--- worker/src/main.rs
+++ worker/src/main.rs
@@ -12,35 +12,35 @@ use std::error::Error;
 use std::io::ErrorKind;
 
 use clap::{App, AppSettings, Arg, ArgMatches};
+use log::Level;
+use log::LevelFilter;
 use log4rs::append::console::ConsoleAppender;
 use log4rs::append::file::FileAppender;
 use log4rs::config::{Appender, Config, Logger, Root};
 use log4rs::encode::pattern::PatternEncoder;
-use log::Level;
-use log::LevelFilter;
 
 use crate::defaultnormaliser::DefaultNormaliser;
 use crate::downloader::DefaultDownloader;
 use crate::extractor::html::{HTMLExtractorBase, HTMLLinkExtractor};
-use crate::filter::filter::{Whitelist, Blacklist, NoFilter};
+use crate::filter::filter::{Blacklist, NoFilter, Whitelist};
 use crate::rmqredis::RMQRedisManager;
 use crate::task::Task;
+use crate::traits::Filter;
 use crate::void::Void;
 use crate::worker::Worker;
-use crate::traits::Filter;
 
+mod archive;
+mod defaultnormaliser;
 mod downloader;
-mod extractor;
 mod errors;
+mod extractor;
+mod filter;
 mod rmqredis;
 mod split;
 mod task;
 mod traits;
 mod void;
 mod worker;
-mod archive;
-mod defaultnormaliser;
-mod filter;
 
 /// Create and return log4rs-config with some default values
 fn get_log4rs_config(log_path: &str, default_log_level: LevelFilter) -> log4rs::config::Config {
@@ -60,7 +60,12 @@ fn get_log4rs_config(log_path: &str, default_log_level: LevelFilter) -> log4rs::
     Config::builder()
         .appender(Appender::builder().build("stdout", Box::new(stdout)))
         .appender(Appender::builder().build("logfile", Box::new(logfile)))
-        .build(Root::builder().appender("stdout").appender("logfile").build(default_log_level))
+        .build(
+            Root::builder()
+                .appender("stdout")
+                .appender("logfile")
+                .build(default_log_level),
+        )
         .unwrap()
 }
 
@@ -139,7 +144,7 @@ fn main() -> Result<(), Box<dyn Error>> {
         Arg::with_name("log-path")
             .short("l")
             .long("log-path")
-            .env("LOG_PATH")
+            .env("SCRAPER_WORKER_LOG_PATH")
             .default_value("worker.log")
             .value_name("PATH")
             .help("Specify the log-file path")
@@ -178,22 +183,23 @@ fn main() -> Result<(), Box<dyn Error>> {
     ).get_matches();
 
     // Load config for logging to stdout and logfile.
-    if let Ok(handle) = log4rs::init_config(
-        get_log4rs_config(
-            args.value_of("log-path").unwrap(),
-            match args.value_of("log-level").unwrap().to_lowercase().as_str() {
-                "error" => LevelFilter::Error,
-                "warn" => LevelFilter::Warn,
-                "info" => LevelFilter::Info,
-                "debug" => LevelFilter::Debug,
-                "trace" => LevelFilter::Trace,
-                "off" => LevelFilter::Off,
-                _ => LevelFilter::Off,
-            })
-    ) {
-        info!("Starting worker module using RabbitMQ({:?}) and redis({:?})",
-              args.value_of("rmq-address").unwrap().to_string(),
-              args.value_of("redis-address").unwrap().to_string());
+    if let Ok(_handle) = log4rs::init_config(get_log4rs_config(
+        args.value_of("log-path").unwrap(),
+        match args.value_of("log-level").unwrap().to_lowercase().as_str() {
+            "error" => LevelFilter::Error,
+            "warn" => LevelFilter::Warn,
+            "info" => LevelFilter::Info,
+            "debug" => LevelFilter::Debug,
+            "trace" => LevelFilter::Trace,
+            "off" => LevelFilter::Off,
+            _ => LevelFilter::Off,
+        },
+    )) {
+        info!(
+            "Starting worker module using RabbitMQ({:?}) and redis({:?})",
+            args.value_of("rmq-address").unwrap().to_string(),
+            args.value_of("redis-address").unwrap().to_string()
+        );
 
         // Construct a worker and its components
         let manager = RMQRedisManager::new(
@@ -205,16 +211,22 @@ fn main() -> Result<(), Box<dyn Error>> {
             args.value_of("rabbitmq-routing-key").unwrap().to_string(),
             args.value_of("rabbitmq-queue").unwrap().to_string(),
             args.value_of("redis-set").unwrap().to_string(),
-        ).expect("Failed to construct RMQRedisManager");
+        )
+        .expect("Failed to construct RMQRedisManager");
         let downloader = DefaultDownloader::new();
         let extractor = HTMLExtractorBase::new(HTMLLinkExtractor::new());
-        let filter: Box<dyn Filter> =
-            if args.value_of("filter-enable").unwrap().parse().unwrap() {
-                match args.value_of("filter-type").unwrap() {
-                    "black" => { Box::new(Blacklist::new(args.value_of("filter-path").unwrap().to_string())) }
-                    "white" | _ => { Box::new(Whitelist::new(args.value_of("filter-path").unwrap().to_string())) }
-                }
-            } else { Box::new(NoFilter) };
+        let filter: Box<dyn Filter> = if args.value_of("filter-enable").unwrap().parse().unwrap() {
+            match args.value_of("filter-type").unwrap() {
+                "black" => Box::new(Blacklist::new(
+                    args.value_of("filter-path").unwrap().to_string(),
+                )),
+                "white" | _ => Box::new(Whitelist::new(
+                    args.value_of("filter-path").unwrap().to_string(),
+                )),
+            }
+        } else {
+            Box::new(NoFilter)
+        };
         let normaliser = DefaultNormaliser;
         let archive = Void;
         let worker = Worker::new(
@@ -230,6 +242,9 @@ fn main() -> Result<(), Box<dyn Error>> {
 
         Ok(())
     } else {
-        Err(Box::new(std::io::Error::new(ErrorKind::Other, "[ERROR] Failed creating logging config")))
+        Err(Box::new(std::io::Error::new(
+            ErrorKind::Other,
+            "[ERROR] Failed creating logging config",
+        )))
     }
 }
