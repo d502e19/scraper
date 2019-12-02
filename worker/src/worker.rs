677df37d94f26116c9aff56d696c5b91fec5e107
cogitantium <falke.cs@gmail.com>
2019-12-02 14:51:33 +0100

    [influx] Add functionality to track count of links in task processing

--- worker/src/worker.rs
+++ worker/src/worker.rs
@@ -7,7 +7,7 @@ use crate::task::Task;
 use crate::traits::{Archive, Downloader, Extractor, Filter, Manager, Normaliser, TaskProcessResult};
 use std::time::{SystemTime, UNIX_EPOCH};
 use std::ops::Sub;
-use crate::metrics::influx_client::{InfluxClient, get_timestamp_millis, MetricSession};
+use crate::metrics::influx_client::{InfluxClient, get_timestamp_millis, TimeSession, CountSession};
 use influx_db_client::{Points, Point, Value};
 
 /// A worker is the web crawler module that resolves tasks. The components of the worker
@@ -58,11 +58,12 @@ impl<S, D> Worker<S, D> {
     pub fn start(&self, influxdb_client: Option<InfluxClient>) {
         info!("Worker {} has started", self.name);
         self.manager.subscribe(&|task| {
-            let mut metric_session = MetricSession::new("worker_processing_time", &self.name);
+            let mut time_session = TimeSession::new("worker_processing_time", &self.name);
+            let mut count_session = CountSession::new("worker_processing_count", &self.name);
 
             info!("Worker {} received task {}", self.name, task.url);
 
-            metric_session.add_data_point("receive_task_time");
+            time_session.add_time_field("receive_task_time");
 
             match self.downloader.fetch_page(&task) {
                 Err(e) => {
@@ -70,7 +71,7 @@ impl<S, D> Worker<S, D> {
                     return TaskProcessResult::from(e);
                 }
                 Ok(page) => {
-                    metric_session.add_data_point("download_task_time");
+                    time_session.add_time_field("download_task_time");
 
                     match self.extractor.extract_content(page, &task.url) {
                         Err(e) => {
@@ -78,31 +79,35 @@ impl<S, D> Worker<S, D> {
                             return TaskProcessResult::from(e);
                         }
                         Ok((mut urls, data)) => {
-                            metric_session.add_data_point("extract_task_time");
+                            time_session.add_time_field("extract_task_time");
+                            count_session.add_first_count_field("extracted_links", urls.len() as i64);
 
                             // Archiving
                             if let Err(e) = self.archive.archive_content(data) {
                                 error!("{} failed archiving some data. {}", self.name, e);
                                 return TaskProcessResult::from(e);
                             }
-                            metric_session.add_data_point("archive_task_time");
+                            time_session.add_time_field("archive_task_time");
 
                             // Normalising urls
                             let tasks: Vec<Task> = self.normaliser.normalise(urls)
                                 .drain(..)
                                 .map(|url| Task { url })
                                 .collect();
-
-                            metric_session.add_data_point("normalise_task_time");
+                            time_session.add_time_field("normalise_task_time");
+                            count_session.add_count_field("extracted_links", tasks.len() as i64);
 
                             let filtered_tasks = self.filter.filter(tasks);
 
-                            metric_session.add_data_point("filter_task_time");
+                            time_session.add_time_field("filter_task_time");
+                            count_session.add_count_field("filtered_links", filtered_tasks.len() as i64);
 
                             // Cull tasks that have already been submitted once, then submit the new tasks
                             match self.manager.cull_known(filtered_tasks) {
                                 Ok(new_tasks) => {
-                                    metric_session.add_data_point("culling_task_time");
+                                    time_session.add_time_field("culling_task_time");
+                                    count_session.add_count_field("culled_links", new_tasks.len() as i64);
+
 
                                     if let Err(e) = self.manager.submit(new_tasks) {
                                         error!("{} failed submitting new tasks to the manager. {}", self.name, e);
@@ -116,7 +121,8 @@ impl<S, D> Worker<S, D> {
                             }
 
                             if let Some(client) = &influxdb_client {
-                                metric_session.write_point(client);
+                                time_session.write_point(client);
+                                count_session.write_point(client);
                             }
 
                             return TaskProcessResult::Ok;
